c2270dcac8c15acb28d3d7912f5bfcb0bebb55b6
GiaBaone <102190252@sv1.dut.udn.vn>
2021-10-08 11:18:18 +0700

    closest-pair-poit 8.10.21

---
+++ math-labday5/closet-pair-point.py
@@ -0,0 +1,44 @@
+import math
+
+def brute(p, c=float("inf")): #thuật toán brutr
+    if len(p) < 2: return c
+    else:
+        f = p[0]
+        del p[0]
+        return brute(p, min([c, min([dist(f, x) for x in p])]))
+        ## combine the first element of p with every other, in a recursive call.
+
+def divide(p): #chia tập hợp điểm thành 2 tập con có kích thước gần bằng nhau
+    if len(p) < 4: ## |p| < 4, 
+        return brute(p)
+    else:
+        mid = len(p)//2;
+        minimum = min([divide(p[:mid]), divide(p[mid:])]) # computes the answers for the closest points strictly in only one of the regions.
+        near_mid = list(filter(lambda point: point.x > p[mid].x - minimum and point.x < p[mid].x + minimum, p))
+        # above, I have used the Python filter function to determine which of the points in P are near the line we found.
+        # that is, if the candidate distance is called minimum, and the x of line a is xa, points whose x is in the interval [xa - minimum; xa + minimum]
+        return min([brute(near_mid), minimum])
+
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def sqr(x):
+    return x*x
+
+def dist(a, b):
+    return sqr(a.x - b.x) + sqr(a.y - b.y) 
+
+N = int(input("Give the number of points (N): "))
+p = []
+
+for i in range(N):
+    x, y = [int(x) for x in input("x y: ").split()]
+    p.append(point(x, y))
+
+P = sorted(p, key=lambda point: point.x) #sắp xếp các điểm theo toạ độ x
+
+
+print("%.6f" % math.sqrt(divide(P)))
+
---
+++ math-labday5/closet-pair-point1.py
@@ -0,0 +1,71 @@
+import math
+
+#khoảng cách giữa các điểm
+def lenght(a,b):
+	return (math.sqrt( ((a[0]-b[0])**2)+ ((a[1]-b[1])**2)) )
+
+#bruteforce để tìm kiếm nửa sai lệch ngắn nhất.
+def halves_search(a,p,q,m):
+	## nếu mảng bao gồm hai điểm =>tìm thấy khoảng cách và so sánh nó với điểm gần nhất
+	if len(a)==2:
+		dist=lenght(a[0],a[1])
+		if dist<m:
+			p,q=a[0],a[1]
+			m=dist
+    #nếu mảng nhiều hơn 2 => tìm khoảng cách giữa tất cả và so sánh chúng với cái ngắn nhất
+	else:
+		for x in range(0,len(a)-1):
+			for y in range (x+1, len(a)):
+				if m>lenght(a[x],a[y]):
+					m=lenght(a[x],a[y])
+					p,q = a[x],a[y]		
+	return p,q,m
+
+### tìm khoảng cách ngắn nhất của mảng a với p,q là 2 điểm, m là khoảng cách ngắn nhất
+def search_pair(a,p,q,m):
+	l=len(a) #lưu độ dài của mảng a
+	if l<=(3): return (halves_search(a,p,q,m)) #nếu length <=3 sử dụng thuật toán brute force
+
+	#tìm vị trí giữa mảng và chia thành 2 mảng con có kích thước gần bằng nhau
+	midl=l//2
+	midx=a[midl][0]
+	L=a[:midl]
+	R=a[midl:]
+
+	#tìm điểm và tiếp tục chia nhỏ mảng
+	p,q,m=search_pair(L,p,q,m)
+	p,q,m=search_pair(R,p,q,m)
+	p,q,m,=get_strip(a,midx,p,q,m)
+	
+	#trả về kết quả là 2 điểm tạo nên khoảng cách ngắn nhất và khoảng cách ngắn nhất
+	return (p,q,m,)
+		       
+#nhận mảng để đếm
+def get_strip(a,xcoordinate,p,q,m):
+	#khởi tạo một mảng rổng
+	strip = []
+	right,left = xcoordinate+int(m), xcoordinate-int(m)
+	#tìm các điểm phù hợp sao cho k đi quá điểm x
+	for x in a:
+		if x[0]>right: break
+		elif left<=x[0]<=right: strip.append(x)
+	#sắp xếp các điểm theo theo toạ độ x
+	strip.sort(key=lambda x:x[1]) 
+	#kiểm tra khoảng cách trong strip và so sánh khoảng cách ngắn nhất. 
+	for x in range (0,len(strip)):
+		for y in range (x+1,len(strip)):
+			dist= lenght(strip[x],strip[y])
+			if dist<m:
+				p,q=strip[x],strip[y]
+				m=dist
+	return (p,q,m)
+
+#main function/program
+points = [(1,2),(2,3),(4,5)]
+points.sort() #sắp xếp 
+p,q = points[0],points[1] #khởi tạo 2 điểm p,q là 2 điểm đầu tiên của mảng
+m=lenght (points[0],points[1]) #giả sử khoảng cách của 2 điểm đầu tiên là ngắn nhất
+pt,pt2,distance=search_pair(points,p,q,m) 
+print (distance)
+print (pt)
+print (pt2) 
