f93ac41157f57c38b2f3884b370755171039af48
GiaBaone <102190252@sv1.dut.udn.vn>
2021-10-21 15:54:54 +0700

    update convex-hull 21.10.21

--- math-labday5/convex-hull.py
+++ math-labday5/convex-hull.py
@@ -1,39 +1,38 @@
 import random
 import numpy as np
 
-# helpers
 
 def RightTurn(p1, p2, p3):
-	if (p3[1]-p1[1])*(p2[0]-p1[0]) >= (p2[1]-p1[1])*(p3[0]-p1[0]):
-		return False
-	return True
-	
-# Chương trình chính
+    if (p3[1]-p1[1])*(p2[0]-p1[0]) >= (p2[1]-p1[1])*(p3[0]-p1[0]):
+        return False
+    return True
+
+
 def convexHull(P):
-	P.sort()			# Sắp xếp các điểm
-	L_upper = [P[0], P[1]]		# Khởi tạo mảng Lupper
-	# Kiểm tra và tính toán để thu được mảng Lupper 
-	for i in range(2,len(P)):
-		L_upper.append(P[i])
-		while len(L_upper) > 2 and not RightTurn(L_upper[-1],L_upper[-2],L_upper[-3]):
-			del L_upper[-2]
-	L_lower = [P[-1], P[-2]]	# Khởi tạo mảng Lower
-	# Kiểm tra và tính toán để thu được mảng Lower
-	for i in range(len(P)-3,-1,-1):
-		L_lower.append(P[i])
-		while len(L_lower) > 2 and not RightTurn(L_lower[-1],L_lower[-2],L_lower[-3]):
-			del L_lower[-2]
-	del L_lower[0]
-	del L_lower[-1]
-	L = L_upper + L_lower		# Dãy bao bọc cuối cùng
-	return np.array(L)
+    P.sort()
+    L_upper = [P[0], P[1]]
+    for i in range(2, len(P)):
+        L_upper.append(P[i])
+        while len(L_upper) > 2 and not RightTurn(L_upper[-1], L_upper[-2], L_upper[-3]):
+            del L_upper[-2]
+    L_lower = [P[-1], P[-2]]
+    for i in range(len(P)-3, -1, -1):
+        L_lower.append(P[i])
+        while len(L_lower) > 2 and not RightTurn(L_lower[-1], L_lower[-2], L_lower[-3]):
+            del L_lower[-2]
+    del L_lower[0]
+    del L_lower[-1]
+    L = L_upper + L_lower		# Dãy bao bọc cuối cùng
+    return np.array(L)
 
 
 if __name__ == '__main__':
-    points = np.array([[0.2, 0.2],
-                       [0.2, 0.4],
-                       [0.4, 0.4],
-                       [0.4, 0.2],
-                       [0.3, 0.6]])
+    points = np.array([[1, 2],
+                       [2, 4],
+                       [3, 5],
+                       [4, 3],
+                       [5, 4],
+                       [6, 1],
+                       [7, 3]])
     c = convexHull(points)
     print(c)
