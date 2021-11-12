ef9322f9e5e7e8f66882a88ccae8f0307a4d5d16
GiaBaone <102190252@sv1.dut.udn.vn>
2021-11-13 00:04:49 +0700

    simplex-method-two-phase 13.11.21

---
+++ math-labday9/main.py
@@ -0,0 +1,59 @@
+from two_phase import Module as m
+import numpy as np
+import os
+
+
+def main():
+    clearScreen()
+    c, b, cons = [], [], []
+    cons_no = int(input("Enter number of constraints: "))
+    x_no = int(input("Enter number of x: "))
+
+    for i in range(x_no):
+        c_val = float(input("Enter Xr{}: ".format(i+1)))
+        c.append(c_val)
+    for i in range(cons_no):
+        b_val = float(input("Enter b{}: ".format(i+1)))
+        b.append(b_val)
+    for i in range(cons_no):
+        temp = []
+        print("Constraint {}".format(i+1))
+        for i in range(x_no):
+            temp.append(float(input("Enter x{}: ".format(i+1))))
+        cons.append(temp)
+
+    artificialMod = m(c, b, cons)
+
+    print('Original Tableu:')
+    tableu = artificialMod.parseTableu()
+    printTableu(tableu)
+
+    print('Phase One Tableu:')
+    tableu, opt, unbounded = artificialMod.phaseOne_Simplex(tableu)
+    printTableu(tableu)
+
+    print('Phase Two Tableu:')
+    tableu, infeasible, opt, unbounded = artificialMod.phaseTwo_Simplex(tableu)
+    printTableu(tableu)
+
+    print("Is Optimal: ", opt)
+    print("Is Infeasible: ", infeasible)
+    print("Is Unbounded: ", unbounded)
+
+    print('Z(x*) = {}'.format(-tableu[0][0]))
+
+
+def printTableu(tableu):
+    print('----------------------')
+    for row in tableu:
+        print(" ", row)
+    print('----------------------')
+    return
+
+
+def clearScreen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+if __name__ == "__main__":
+    main()
---
+++ math-labday9/two_phase.py
@@ -0,0 +1,189 @@
+class Module:
+
+    def __init__(self, c, b, cons):
+        self.c = c
+        self.b = b
+        self.cons = cons
+
+    def parseTableu(self):
+        c, b, cons = self.c, self.b, self.cons
+        tableu, artificial = [], []
+        m, n = len(cons), len(c)
+        sigma = [0.0]
+
+        c.insert(0, 0.0)
+
+        # init x1-xn =0
+        i = 0
+        while (i < n):
+            sigma.append(0.0)
+            i += 1
+        # init t1-tm =1
+        i = 0
+        while (i < m):
+            artificial.append(0.0)
+            sigma.append(1.0)
+            i += 1
+
+        c.extend(artificial)
+        tableu.append(c)
+        tableu.append(sigma)
+
+        # hscoban #Pan
+        i = 0
+        for eq in cons:
+            eq.insert(0, b[i])
+            eq.extend(artificial)
+            eq[n+1+i] = 1.0
+            tableu.append(eq)
+            i += 1
+
+        # init table
+        i = 0
+        for xi in tableu:
+            if (i > 1):
+                j = 0
+                for xij in xi:
+                    tableu[1][j] -= xij
+                    j += 1
+            i += 1
+        return tableu
+
+    def phaseOne_Simplex(self, tableu):
+        THETA_INFINITE = -1
+        opt, unbounded = False, False
+        n, m = len(tableu[0]), len(tableu) - 2
+
+        while ((not opt) and (not unbounded)):
+            min = 0.0
+            pivotCol = j = 1
+            while(j < (n-m)):
+                cj = tableu[1][j]
+                if (cj < min):
+                    min = cj
+                    pivotCol = j
+                j += 1
+            if min == 0.0:
+                opt = True
+                continue
+            pivotRow = i = 0
+            minTheta = THETA_INFINITE
+            for xi in tableu:
+                if (i > 1):
+                    xij = xi[pivotCol]
+                    if (xij) > 0:
+                        theta = ((xi[0]) / (xij))
+                        if (theta < minTheta) or (minTheta == THETA_INFINITE):
+                            minTheta = theta
+                            pivotRow = i
+                i += 1
+            if minTheta == THETA_INFINITE:
+                unbounded = True
+                continue
+
+            tableu = self.pivotOn(tableu, pivotRow, pivotCol)
+        return tableu, opt, unbounded
+
+    def phaseTwo_Simplex(self, tableu):
+        tableu, opt, unbounded = self.phaseOne_Simplex(tableu)
+
+        infeasible = False
+        sigma = tableu[1][0]
+
+        if (sigma > 0):
+            infeasible = True
+        else:
+            # sigma is equals to zero
+            tableu = self.drive_out_artificial_basis(tableu)
+            m = len(tableu) - 2
+            n = len(tableu[0])
+            n -= m
+            tableu.pop(1)
+            i = 0
+            while (i < len(tableu)):
+                tableu[i] = tableu[i][:n]
+                i += 1
+            tableu, opt, unbounded = self.simplex(tableu)
+
+        return tableu, infeasible, opt, unbounded
+
+    # don hinh
+    def simplex(self, tableu):
+        THETA_INFINITE = -1
+        opt, unbounded = False, False
+        n, m = len(tableu[0]), len(tableu) - 1
+
+        while ((not opt) and (not unbounded)):
+            min = 0.0
+            pivotCol = j = 0
+            while(j < (n-m)):
+                cj = tableu[0][j]
+                if (cj < min) and (j > 0):
+                    min = cj
+                    pivotCol = j
+                j += 1
+            if min == 0.0:
+                opt = True
+                continue
+            pivotRow = i = 0
+            minTheta = THETA_INFINITE
+            for xi in tableu:
+                if (i > 0):
+                    xij = xi[pivotCol]
+                    if xij > 0:
+                        theta = (xi[0] / xij)
+                        if (theta < minTheta) or (minTheta == THETA_INFINITE):
+                            minTheta = theta
+                            pivotRow = i
+                i += 1
+            if minTheta == THETA_INFINITE:
+                unbounded = True
+                continue
+            tableu = pivotOn(tableu, pivotRow, pivotCol)
+        return tableu, opt, unbounded
+
+    def pivotOn(self, tableu, row, col):
+        j = 0
+        pivot = tableu[row][col]
+        for x in tableu[row]:
+            tableu[row][j] = (tableu[row][j]) / (pivot)
+            j += 1
+        i = 0
+        for xi in tableu:
+            if i != row:
+                ratio = xi[col]
+                j = 0
+                for xij in xi:
+                    xij -= ratio * tableu[row][j]
+                    tableu[i][j] = xij
+                    j += 1
+            i += 1
+        return tableu
+
+    def drive_out_artificial_basis(self, tableu):
+        n = len(tableu[0])
+        j = n - 1
+        isbasis = True
+        while(j > 0):
+            found = False
+            i = -1
+            row = 0
+            for xi in tableu:
+                i += 1
+                if (xi[j] == 1):
+                    if (found):
+                        isbasis = False
+                        continue
+                    elif (i > 1):
+                        row = i
+                        found = True
+                elif (xi[0] != 0):
+                    isbasis = False
+                    continue
+            if (isbasis and found):
+                if (j >= n):
+                    tableu = pivotOn(tableu, row, j)
+                else:
+                    return tableu
+            j -= 1
+        return tableu
